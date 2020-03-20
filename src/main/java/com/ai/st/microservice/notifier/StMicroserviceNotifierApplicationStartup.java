5db52931437d21f88155e16edf17a697460c610c
Jhon F. Rondon <jhonfr29@gmail.com>
2020-03-20 12:49:27 -0500

    chore: Modify profile AWS

--- src/main/java/com/ai/st/microservice/notifier/StMicroserviceNotifierApplicationStartup.java
+++ src/main/java/com/ai/st/microservice/notifier/StMicroserviceNotifierApplicationStartup.java
@@ -1,7 +1,5 @@
 package com.ai.st.microservice.notifier;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -9,11 +7,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class StMicroserviceNotifierApplicationStartup implements ApplicationListener<ContextRefreshedEvent> {
 
-	private static final Logger log = LoggerFactory.getLogger(StMicroserviceNotifierApplicationStartup.class);
-
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
-		log.info("ST - Loading ... ");
+
 	}
 
 }
